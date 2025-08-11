f7c091e41c4a0ce1946363403a0c153356179b6d
Tomás Cichero <martoxdlol@gmail.com>
2025-08-11 19:44:27 -0300

    copy changelog

--- packages/build-package/src/copyfiles.js
+++ packages/build-package/src/copyfiles.js
@@ -3,4 +3,5 @@ import { copyFile } from "fs/promises";
 export async function copyFiles() {
     await copyFile('../../LICENSE', './dist/LICENSE');
     await copyFile('./README.md', './dist/README.md');
+    await copyFile('./CHANGELOG.md', './dist/CHANGELOG.md').catch(() => undefined);
 }
