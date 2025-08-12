841e7ff641e07b90f65e1087c1979e6f4bba5d4a
Tomás Cichero <martoxdlol@gmail.com>
2025-08-12 11:23:47 -0300

    remove logic created by cursor

--- packages/build-package/src/index.js
+++ packages/build-package/src/index.js
@@ -2,7 +2,6 @@ import { readFile } from 'node:fs/promises'
 import { buildBundles } from './build.js'
 import { copyFiles } from './copyfiles.js'
 import { writeDistPackageJson } from './package.js'
-import { publishPackage } from './publish.js'
 
 /**
  * The parsed contents of the root `package.json` file.
@@ -14,14 +13,3 @@ const pkg = JSON.parse(await readFile('./package.json', 'utf-8'))
 await buildBundles(pkg)
 await writeDistPackageJson(pkg)
 await copyFiles()
-
-// Check if we should publish (if --publish flag is passed)
-const shouldPublish = process.argv.includes('--publish')
-if (shouldPublish) {
-    try {
-        await publishPackage('./dist')
-    } catch (error) {
-        console.error('❌ Publishing failed:', error.message)
-        process.exit(1)
-    }
-}
